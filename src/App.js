125c80fb43e52f84777763e3ea1f6df78b9ef862
rizkiprass <rizkiprass@gmail.com>
2023-06-16 19:31:35 +0700

    [Checkpoint] Read data from db

--- src/App.js
+++ src/App.js
@@ -1,30 +1,98 @@
-import React, { useEffect, useState }from 'react'
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-function App() {
+function Home() {
+  const [backendData, setBackendData] = useState({ users: [] });
 
-    const [backendData, setBackendData] = useState([{}])
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response.json())
+      .then((data) => {
+        setBackendData(data);
+      });
+  }, []);
 
-    useEffect(() => {
-        fetch("/api").then(
-            response => response.json()
-        ).then(
-            data => {
-                setBackendData(data)
-            }
-        )
-    }, [])
   return (
     <div>
-
-        {(typeof backendData.users === 'undefined') ? (
-            <p>Loading ... </p>
-        ) : (
-            backendData.users.map((user, i) => (
-                <p key={i}>{user}</p>
-            ))
-        )}
+      {backendData.users.length === 0 ? (
+        <p>Loading...</p>
+      ) : (
+        backendData.users.map((user, i) => <p key={i}>{user}</p>)
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+function DB() {
+  const [dbData, setDBData] = useState([]);
+
+  useEffect(() => {
+    fetch("/db")
+      .then((response) => response.json())
+      .then((data) => {
+        setDBData(data.data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>DB</h2>
+      {dbData.length === 0 ? (
+        <p>Loading...</p>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>Movie Name</th>
+              <th>Movie Review</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dbData.map((row) => (
+              <tr key={row.id}>
+                <td>{row.movieName}</td>
+                <td>{row.movieReview}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+}
+
+function App() {
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response.json())
+      .then((data) => {
+        setBackendData(data);
+      });
+  }, []);
+
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/db">DB</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/db" element={<DB />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
